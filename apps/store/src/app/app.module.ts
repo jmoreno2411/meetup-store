3fd5e2c5f0fe6990547033c5fbec72f48ef8b7a5
jmoreno2411 <97634678+jmoreno2411@users.noreply.github.com>
2023-10-17 13:46:04 +0200

    feat(store): remove nx welcome component

--- apps/store/src/app/app.module.ts
+++ apps/store/src/app/app.module.ts
@@ -3,12 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
